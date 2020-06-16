3242c080f5acc31fa0ea0d138ae5a0ffbee894d9
orcascope <30562546+orcascope@users.noreply.github.com>
2020-06-16 10:20:22 +0530

    Update PreAuthenticationFilter.java
    Added code to decrypt the token, authenticate.

--- core/src/main/java/com/helicalinsight/admin/filter/PreAuthenticationFilter.java
+++ core/src/main/java/com/helicalinsight/admin/filter/PreAuthenticationFilter.java
@@ -16,14 +16,17 @@
 
 package com.helicalinsight.admin.filter;
 
+import com.helicalinsight.auth.customAuth.CipherUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.regex.Pattern;
 
 /**
  * This is sub class of spring framework's UsernamePasswordAuthenticationFilter,
@@ -47,6 +50,30 @@ public class PreAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
             AuthenticationException {
+        
+        //the below code decrypts the encrypted j_username
+        if ( request.getParameter("j_username").length()>20) {
+            String authTokenUser = null;
+            String authTokenPass = null;
+            final String encryptedToken = request.getParameter("j_username");
+            final String token = CipherUtils.decrypt(encryptedToken);
+            final String[] userDetailsParams = token.split(Pattern.quote("|"));
+            for (int i = 0; i < userDetailsParams.length; ++i) {
+                final String[] detail = userDetailsParams[i].split(Pattern.quote("="));
+                if (detail[0].equalsIgnoreCase("username") && detail.length == 2) {
+                    authTokenUser = detail[1];
+                }
+                else if (detail[0].equalsIgnoreCase("password") && detail.length == 2) {
+                    authTokenPass = detail[1];
+                }
+            }
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(authTokenUser, authTokenPass);
+            logger.debug(authTokenUser + " is authenticated ? " +  authRequest.isAuthenticated());
+            setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        }
+        //End decrypting the username
+
         logger.debug("Attempting for authentication. " + "j_username = " + request.getParameter("j_username") + ", " +
                 "j_password = [*****]");
         return super.attemptAuthentication(request, response);
